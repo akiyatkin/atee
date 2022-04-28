9b0ad935178987d572f61f8ba5ab9a22c9e907fc
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2022-04-28 16:24:23 +0400

    fix

--- controller/init.js
+++ controller/init.js
@@ -1,14 +1,15 @@
 
 const sw = async () => {
 	if (!navigator.serviceWorker) return
-	const AccessData = await import('/-controller/access', {assert: { type: 'json' }})
+	//const { default: access_data } = await import('/-controller/access', {assert: { type: 'json' }})
+	const access_data = await fetch('/-controller/access').then(res => res.json())
 	navigator.serviceWorker.register('/-controller/sw?t', { scope:'/' });
 	navigator.serviceWorker.addEventListener('message', event => {
 		console.log('New version is ready. Reload please.', event.data)
     })
 	if (navigator.serviceWorker.controller) { //В первый раз данные придут с кодом воркера
 		await navigator.serviceWorker.ready
-		navigator.serviceWorker.controller.postMessage(AccessData)
+		navigator.serviceWorker.controller.postMessage(access_data)
 	}
 }
 
