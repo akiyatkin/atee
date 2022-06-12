083cad28bd9c904af42fc436ce648d8ebb75d7a9
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2022-06-12 11:02:54 +0400

    dev

--- controller/sw.js
+++ controller/sw.js
@@ -74,9 +74,10 @@ this.addEventListener('fetch', event => {
 	let request = new Request(url, options)
 
 	//console.log(event.request.url, UPDATE_TIME)
-	let response = fetch(request).then(response => {
-		response.headers.set('Cache-Control', 'no-store')
-	})
+	let response = fetch(request)
+	// let response = fetch(request).then(response => {
+	// 	response.headers.set('Cache-Control', 'no-store')
+	// })
 	
 	event.respondWith(response)
 });
