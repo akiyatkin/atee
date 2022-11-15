06a7554f538f7da2d8d3ee1416d0cc81aa941868
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2022-11-15 14:38:13 +0400

    csv

--- mail/index.js
+++ mail/index.js
@@ -1,31 +1,18 @@
 import nodemailer from 'nodemailer'
 import fs from 'fs/promises'
 import config from '@atee/config'
-import cproc from '@atee/cproc'
+import csv from '@atee/csv'
 
 const conf = await config('mail')
-
 const transport = nodemailer.createTransport(conf.nodemailer)
-await fs.mkdir('data/auto/', { recursive: true }).catch(e => null)
 
-const file = 'data/auto/mail.csv'
-if (await fs.access(file).then(e => false).catch(e => true)) {
-	await fs.writeFile(file, '﻿Дата;From;To;Тема;Письмо'+"\n").catch(e => console.log(e))
-}
-const tocsv = (strings, ...values) => {
-	values = values.map(v => {
-		v = v.replaceAll("\n",' ')
-		v = v.replaceAll(";",':')
-		return v	
-	})
-	return String.raw({ raw: strings }, ...values);
-}
+await fs.mkdir('data/auto/mail/', { recursive: true }).catch(e => null)
+
 
 const mail = {
 	saveSend: opt => {
-		cproc(conf, 'saveSend', async () => {
-			await fs.appendFile(file, tocsv`${new Date().toLocaleString()};${opt.from};${opt.to};${opt.subject};${opt.html};`+"\n").catch(e => console.log(e))
-		}, Date.now())
+		opt.date = new Date().toLocaleString()
+		csv('data/auto/mail/mail-v1.csv', opt)
 		const info = transport.sendMail(opt).catch(err => console.log('mail.saveSend', err))
 		return info
 	},
