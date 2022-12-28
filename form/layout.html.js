5ad05003e0911f0536531a9967a954e4f17564fd
Anton Kiyatkin <aky@list.ru>
2022-12-28 16:29:37 +0400

    better layout for utms

--- form/layout.html.js
+++ form/layout.html.js
@@ -1,7 +1,7 @@
 export const UTMS = (utms) => `
 	<table style="text-align:left;">
 		<thead style="background-color: #eee;">
-			<tr><th>Дата</th><th>Откуда</th><th>Куда</th></tr>
+			<tr><th>Дата</th><th>Откуда</th><th>Куда</th><td>UTM</td></tr>
 		</thead>
 		<tbody>
 			${utms.map(utm).join('')}
@@ -13,8 +13,17 @@ const ddmm = new Intl.DateTimeFormat("ru-RU", { dateStyle: 'short', timeStyle: '
 const utm = (utm) => `
 	<tr>
 		<td style="vertical-align: top"><nobr>${ddmm.format(new Date(utm.time))}</nobr></td>
-		<td style="vertical-align: top"><nobr title="${utm.ref.pathname}${utm.ref.search}">${utm.ref.host}</nobr></td>
-		<td style="vertical-align: top">${utm.url.pathname}${utm.url.search}</td>
+		<td style="vertical-align: top"><a href="${utm.ref.href}" title="${utm.ref.href}">${utm.ref.host}</a></td>
+		<td style="vertical-align: top"><a href="http://${utm.url.host}${utm.url.pathname}" title="${utm.url.href}">${utm.url.host}${utm.url.pathname}</a></td>
+		<td style="vertical-align: top">
+			${[
+				utm.url.searchParams.get('utm_source')||'',
+				utm.url.searchParams.get('utm_medium')||'',
+				utm.url.searchParams.get('utm_campaign')||'',
+				utm.url.searchParams.get('utm_term')||'',
+				utm.url.searchParams.get('utm_content')||''
+			].join(' ')}
+		</td>
 	</tr>
 `
 
