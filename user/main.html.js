9a6555ecaecae28cb64ae1c4b745ec50576c967f
Anton Kiyatkin <aky@list.ru>
2023-05-18 09:57:53 +0400

    tpl

--- user/main.html.js
+++ user/main.html.js
@@ -1,11 +1,13 @@
-export const HEADER = (data, env) => `
+const tpl = {}
+
+tpl.HEADER = (data, env) => `
 	<div style="display: flex; justify-content: space-between;">
 		<a href="/${env.bread.root}">Личный кабинет</a>
 		<a href="/">${env.host}</a>
 	</div>
 `
 
-export const ROOT = (data, env) => data.result ? `
+tpl.ROOT = (data, env) => data.result ? `
 	<h1>Личный кабинет</h1>
 	<p>${data.user.email || ''}</p>
 	<div style="display: grid;">
@@ -15,9 +17,12 @@ export const ROOT = (data, env) => data.result ? `
 	</div>
 ` : `<p>Ошибка</p>`
 
-export const FOOTER = (data, env) => `
+tpl.FOOTER = (data, env) => `
 	<ul>
 		<li><a href="list">Список пользователей</a></li>
 		<li><a href="settings">Настройки</a></li>
 	</ul>
 `
+
+
+export default tpl
