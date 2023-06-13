36065dd71313302a6c47bf26e5f640b459cf4b06
Anton Kiyatkin <aky@list.ru>
2023-06-13 13:02:07 +0400

    load

---
+++ inactive/load.js
@@ -0,0 +1,18 @@
+/*
+	В шапке должен быть скрипт
+	<script class="cls" data-src="...
+*/
+export default cls => {
+	const script = document.head.getElementsByClassName(cls)[0]
+	if (script.load) return script.load
+	let resolve, reject
+	script.load = new Promise((rs, rj) => {
+		if (script.src) return rs()
+		resolve = rs
+		reject = rj
+	})
+	script.addEventListener('load', resolve)
+	script.addEventListener('error', reject)
+	script.src = script.dataset.src
+	return script.load
+}
