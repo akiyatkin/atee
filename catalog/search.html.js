18ef1ed3488938e0a2848fe0733718fba784b7e7
Anton Kiyatkin <aky@list.ru>
2023-06-28 14:59:25 +0400

    fix pagt

--- catalog/search.html.js
+++ catalog/search.html.js
@@ -1,19 +1,19 @@
 import cards from "/-catalog/cards.html.js"
 import html from "/-words/html.js"
 import dataenv from "/-words/dataenv.js"
-import {words} from "/-words/words.js"
+import words from "/-words/words.js"
 import ti from "/-words/ti.js"
 import links from "/-catalog/links.html.js"
-const search = {}
-export default search
+const tpl = {}
+export default tpl
 
-search.ROOT = () => `
+tpl.ROOT = () => `
 	<div id="CATGROUPS"></div>
 	<div id="page"><div id="CATPAG"></div></div>
 	<div id="CATLIST"></div>
 `
-search.GROUPS = (data, env) => data.result ? `
-	${search.title(data, env)}
+tpl.GROUPS = (data, env) => data.result ? `
+	${tpl.title(data, env)}
 	<div class="grid">
 		<style>
 			${env.scope} > .grid {
@@ -29,19 +29,20 @@ search.GROUPS = (data, env) => data.result ? `
 			}
 		</style>
 		<div style="display: grid; height: max-content; gap: 0.4em;">
-			${ti.fi(data.childs.length, search.showgroups(data, env))}
+			${ti.fi(data.childs.length, tpl.showgroups(data, env))}
 		</div>
 		<div id="FILTERS"></div>
 	</div>
 ` : `<h1>${data.root_title}</h1>`
-search.PAGINATION = (data, env) => ti.bs(data.result) && `
-	${search.pag(data, env, 'none')}
+tpl.PAGINATION = (data, env) => ti.bs(data.result) && `
+	${tpl.pag(data, env, 'none')}
 `
-search.LIST = (data, env) => data.result ? `
-	${data.list.length ? search.listcards(data, env) : '<div style="margin: 2rem 0; ">К сожалению, ничего не найдено.</div>'}
-	${data.countonpage == data.list.length ? search.pag(data, env) : ''}
+tpl.LIST = (data, env) => data.result ? `
+	${data.list.length ? tpl.listcards(data, env) : '<div style="margin: 2rem 0; ">К сожалению, ничего не найдено.</div>'}
+	${data.countonpage == data.list.length ? tpl.pag(data, env) : ''}
 ` : '<div style="margin: 2rem 0; ">Данные не найдены</div>'
-search.pag = (data, env, scroll) => `
+
+tpl.pag = (data, env, scroll) => `
 	<div class="pagination">
 		<style>
 			${env.scope} .pagination {
@@ -68,8 +69,8 @@ search.pag = (data, env, scroll) => `
 				}
 			}
 		</style>
-		<div class="begin">${search.pag[data.pagination.page != 1 ? 'link' : 'disabled'](data, env, scroll, 'В начало', 1)}</div>
-		<div class="backward">${search.pag[data.pagination.page != 1 ? 'link' : 'disabled'](data, env, scroll, 'Назад', data.pagination.page - 1)}</div>
+		<div class="begin">${tpl.pagt[data.pagination.page != 1 ? 'link' : 'disabled'](data, env, scroll, 'В начало', 1)}</div>
+		<div class="backward">${tpl.pagt[data.pagination.page != 1 ? 'link' : 'disabled'](data, env, scroll, 'Назад', data.pagination.page - 1)}</div>
 		<div class="page" title="" style="${data.pagination.last == 1 ? 'opacity:0.3' : ''}">${data.pagination.page}</div>
 		<script>
 			(async page => {
@@ -81,13 +82,13 @@ search.pag = (data, env, scroll) => `
 				
 			})(document.currentScript.previousElementSibling)
 		</script>
-		<div class="forward">${search.pag[data.pagination.last > data.pagination.page ? 'link' : 'disabled'](data, env, scroll, 'Дальше', data.pagination.page + 1)}</div>
+		<div class="forward">${tpl.pagt[data.pagination.last > data.pagination.page ? 'link' : 'disabled'](data, env, scroll, 'Дальше', data.pagination.page + 1)}</div>
 		<div class="paglong">${data.count} ${words(data.count,'модель','модели','моделей')}</div>
 	</div>
 `
 
-
-search.pag.link = (data, env, scroll = '', title, page) => `
+tpl.pagt = {}
+tpl.pagt.link = (data, env, scroll = '', title, page) => `
 	<a data-scroll="${scroll}">${title}</a>
 	<script>
 		(async a => {
@@ -106,20 +107,20 @@ search.pag.link = (data, env, scroll = '', title, page) => `
 				return '?' + reqs.join('&')
 			}
 			onload(() => {
-				a.href = "${env.crumb.child}" + getreq('${env.bread.get.m || ''}', '${page}') + "${!scroll?'#page':''}"	
+				a.href = "${env.crumb}" + getreq('${env.bread.get.m || ''}', '${page}') + "${!scroll?'#page':''}"	
 			})
 		})(document.currentScript.previousElementSibling)
 	</script>
 `
-search.pag.disabled = (data, env, scroll, title) => `
+tpl.pagt.disabled = (data, env, scroll, title) => `
 	<span style="opacity: 0.3">${title}</span>
 `
-search.listcards = (data, env) => `	
+tpl.listcards = (data, env) => `	
 	<div style="margin-top:1rem; margin-bottom: 2rem">
 		${cards.LIST(data, env)}
 	</div>
 `
-search.showgroups = (data, env) => `
+tpl.showgroups = (data, env) => `
 	<style>
 		${env.scope} .mute {
 			opacity: 0.3;
@@ -134,13 +135,13 @@ search.showgroups = (data, env) => `
 			opacity: 0.9;
 		}
 	</style>
-	${data.childs.map(g => search.group(data, env, g)).join('')}
+	${data.childs.map(g => tpl.group(data, env, g)).join('')}
 `
 const groupclass = (data, env, group) => {
 	const selected = data.md.group?.[group.group_nick]
 	return `class="${selected ? 'selected' :''} ${group.mute ? 'mute' :''}"`
 }
-search.group = (data, env, group) => `
+tpl.group = (data, env, group) => `
 	<div>
 		<a ${groupclass(data, env, group)}
 			data-scroll="none"
@@ -148,7 +149,7 @@ search.group = (data, env, group) => `
 	</div>
 `
 
-search.title = (data, env) => html`
+tpl.title = (data, env) => html`
 	<style>
 		${env.scope} a.clearlink {
 			white-space: normal; 
@@ -176,34 +177,34 @@ search.title = (data, env) => html`
 	</style>
 	<div style="float:right; margin-top:1rem">${data.type}</div>
 	<div style="float:left; margin-top:1rem">
-		${(!data.path.length && !data.md.m) ? '<a href="/"><br></a>' : search.parenttitle(data, env)}
+		${(!data.path.length && !data.md.m) ? '<a href="/"><br></a>' : tpl.parenttitle(data, env)}
 	</div>
 	
 	<h1 style="display: flex; clear:both; gap:0 0.6ch; flex-wrap:wrap">
 		${data.title.group_title}
-		${!data.brand || search.titlepart(data, env, 'brand', data.brand.brand_title)}
-		${!data.md.search || search.titlepart(data, env, 'search', data.md.search)}
+		${!data.brand || tpl.titlepart(data, env, 'brand', data.brand.brand_title)}
+		${!data.md.search || tpl.titlepart(data, env, 'search', data.md.search)}
 		${!data.md.more || Object.keys(data.md.more).map(prop_nick => {
 			const values = data.md.more[prop_nick]
 			const prop = data.mdprops[prop_nick]
 			if (prop.type == 'value') {
 				return Object.keys(values).map(value_nick => {
 					const value = data.mdvalues[value_nick]
-					return search.choice.just(data, env, prop, value)
+					return tpl.choice.just(data, env, prop, value)
 				}).join(', ')
 			} else {
 				return Object.keys(values).map(value_nick => {
 					if (value_nick == 'from' || value_nick == 'upto') {
-						return search.choice.just(data, env, prop, {value_nick, value_title: (value_nick == 'from' ? 'от ' : 'до ') + values[value_nick] + ' ' + (prop.opt.unit)})
+						return tpl.choice.just(data, env, prop, {value_nick, value_title: (value_nick == 'from' ? 'от ' : 'до ') + values[value_nick] + ' ' + (prop.opt.unit)})
 					} else {
-						return search.choice.just(data, env, prop, {value_nick, value_title: value_nick})
+						return tpl.choice.just(data, env, prop, {value_nick, value_title: value_nick})
 					}
 				}).join(', ')
 			}
 		})}
 	</h1>
 `
-search.choice = {
+tpl.choice = {
 	"just": (data, env, prop, value) => `
 		<a title="${prop.prop_title}" data-scroll="none" class="clearlink" 
 			href="${env.crumb}${links.addm(data)}more.${prop.prop_nick}.${value.value_nick}">
@@ -212,17 +213,17 @@ search.choice = {
 		</a>
 	`
 }
-search.titlepart = (data, env, part, value) => `
+tpl.titlepart = (data, env, part, value) => `
 	<a data-scroll="none" class="clearlink" 
 		href="${env.crumb}${links.addm(data)}${part}">
 		<span class="value">${value}</span>
 		<span class="krest" style="font-size:1rem; line-height: 2rem;">✕</span>
 	</a>
 `
-search.parenttitle = (data, env) => `
-	<a data-scroll="none" href="${env.crumb}${search.toplink(data, env)}">${data.parent.group_title}</a>
+tpl.parenttitle = (data, env) => `
+	<a data-scroll="none" href="${env.crumb}${tpl.toplink(data, env)}">${data.parent.group_title}</a>
 `
-search.toplink = (data, env) => {
+tpl.toplink = (data, env) => {
 	if (data.parent.parent_id) {
 		return '/' + data.parent.group_nick+links.setm(data)
 	} else {
