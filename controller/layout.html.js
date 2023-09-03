b6eccdb3e47a88bf6f8662a341af6152a0d354e9
Anton Kiyatkin <aky@list.ru>
2023-09-03 05:56:17 +0400

    table responsive

--- controller/layout.html.js
+++ controller/layout.html.js
@@ -4,7 +4,7 @@ export default controller
 controller.HEAD = (data, env) => 
 `<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<base href="${env.crumb == '/' ? '/' : env.crumb + '/'}">
+		<!-- <base href="${env.crumb == '/' ? '/' : env.crumb + '/'}"> -->
 		<script>//Делаем SPA переходы
 			const isSuitable = a => {
 				const search = a.getAttribute('href')
