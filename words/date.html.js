907f547a7baada244759e245b1a216935050eb0d
Anton Kiyatkin <aky@list.ru>
2023-10-15 16:47:37 +0400

    fix

--- words/date.html.js
+++ words/date.html.js
@@ -6,8 +6,8 @@ date.dmy = (time) => {
 }
 const intlhi = new Intl.DateTimeFormat("ru-RU", {
 	year: "numeric",
-  month: "2-digit",
-  day: "2-digit",
+ 	month: "2-digit",
+	day: "2-digit",
 	hour: '2-digit',
   	minute: '2-digit'
 })
