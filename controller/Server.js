cda0446eab96f25716c9121fac360ef6618bab05
Антон Кияткин <aky@list.ru>
2025-08-22 15:36:20 +0400

    dev push

--- controller/Server.js
+++ controller/Server.js
@@ -132,7 +132,9 @@ const Server = {
 					//return view.err('', 301)
 				} else {
 					//if (json.push?.length) response.setHeader('Link', json.push.join(','));
-					if (json.push) {
+
+					if (json.push?.length) {
+						console.log(json.push)
 						response.writeEarlyHints({
 							'link': json.push
 						})
