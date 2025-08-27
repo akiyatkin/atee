5227a10d0d79e50ecb91e561caea80f1448f5548
Антон Кияткин <aky@list.ru>
2025-08-27 11:34:11 +0400

    timeout

--- inactive/inActive.js
+++ inactive/inActive.js
@@ -1,32 +1,29 @@
 import isViewport from "./isViewport.js"
 const inActive = new Promise(resolve => {
-    //const base = document.activeElement
-    
-    const init = () => {
-        // document.body.removeEventListener('click', init)
-        // document.body.removeEventListener('mouseover', init)
-        // window.removeEventListener('click', init)
-        window.removeEventListener('mouseover', init)
-        // window.removeEventListener('resize', init)
-        window.removeEventListener('keydown', init)
-        window.removeEventListener('touchstart', init)
-        //window.removeEventListener('scroll', init)
-        //if (base) base.removeEventListener('blur', init)
-        resolve()
-    }
-    setTimeout(() => {
-        // document.body.addEventListener('click', init)
-        // document.body.addEventListener('mouseover', init)
-
-        // window.addEventListener('click', init)
-        window.addEventListener('mouseover', init)
-        // window.addEventListener('resize', init)
-        window.addEventListener('keydown', init)
-        window.addEventListener('touchstart', init)
-        
-        //if (base) base.addEventListener('blur', init)
-        //window.addEventListener('scroll', init)
-    }, 100)
+	//const base = document.activeElement
+	
+	const init = () => {
+		// document.body.removeEventListener('click', init)
+		// document.body.removeEventListener('mouseover', init)
+		// window.removeEventListener('click', init)
+		window.removeEventListener('mouseover', init)
+		// window.removeEventListener('resize', init)
+		window.removeEventListener('keydown', init)
+		window.removeEventListener('touchstart', init)
+		//window.removeEventListener('scroll', init)
+		//if (base) base.removeEventListener('blur', init)
+		resolve()
+	}
+	
+	// document.body.addEventListener('click', init)
+	// document.body.addEventListener('mouseover', init)
+	// window.addEventListener('click', init)
+	window.addEventListener('mouseover', init)
+	// window.addEventListener('resize', init)
+	window.addEventListener('keydown', init)
+	window.addEventListener('touchstart', init)
+	//if (base) base.addEventListener('blur', init)
+	//window.addEventListener('scroll', init)
 })
 
 export { inActive }
