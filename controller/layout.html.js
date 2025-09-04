bb989cf9c08aa21e1964903747a03c5d1327bee8
Антон Кияткин <aky@list.ru>
2025-09-04 11:13:23 +0400

    check cache

--- controller/layout.html.js
+++ controller/layout.html.js
@@ -96,7 +96,8 @@ controller.HEAD = (data, env) =>
 				if (new_access_time) search += '=' + new_access_time
 				location.href = location.pathname + search + location.hash
 			}
-			setTimeout(check, 1000)
+			check()
+			//setTimeout(check, 1000)
 		</script>
 		<script type="module">// Проверка что получен кэш с нужной темой
 			const fromCookie = () => {
