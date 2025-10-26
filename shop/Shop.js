b365ea28a18c6a89efe0e93c00219fd8416e4347
Антон Кияткин <aky@list.ru>
2025-10-26 21:21:52 +0400

    fix

--- shop/Shop.js
+++ shop/Shop.js
@@ -795,7 +795,7 @@ Shop.getModelsByItems = async (db, moditems_ids, partner, props = []) => { //mod
 		-- and (val.value_id is null or val.value_nick != '')
 		-- and (pr.type != 'value' or (val.value_id is not null and val.value_nick != ''))
 		
-		${props.length ? 'and pr.prop_nick in ("' + props.join('","') + '")' : 'and (pr.known != "system"${checkcost})'}
+		${props.length ? 'and pr.prop_nick in ("' + props.join('","') + '")' : 'and (pr.known != "system"' + checkcost + ')'}
 	`))
 
 	const models = Object.groupBy(itemprops, row => key_id_to_model_id[row.key_id])
