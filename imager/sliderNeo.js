351fa0e900b7a395e43f51534bb155498e61292e
Антон Кияткин <aky@list.ru>
2026-04-26 20:03:35 +0400

    fix shift

--- imager/sliderNeo.js
+++ imager/sliderNeo.js
@@ -39,37 +39,69 @@ export default div => {
 		slider.style.scrollBehavior = "auto";
 	}
 	const prev = () => {
-		const length = child.offsetWidth + gap
-		let len = length - ((slider.scrollLeft) % length)
-		if (len < length * 1 / 2) len = length - len
-		else len = 2 * length - len
-		let newval = slider.scrollLeft - len
-		if (newval <= 10) {
-			newval = 0
-			direction = true
+		const slide_width = child.offsetWidth + gap		
+		//console.log(slider.scrollWidth, slider.offsetWidth, slider.scrollLeft)
+		/*
+			slider.scrollWidth = 4000  вся длина скрытая за прокруткой
+			slider.offsetWidth = 1295 видимая часть
+			slider.scrollLeft = 10 сколько уже прокрутили от начала
+			slide_width = 400 - ширина слайда
+			slide_shift = 10 % 400 = 10 - сдвиг от слайда
+			max_scroll = 4000 - 1295
+			newval = slider.scrollLeft = сколько надо прокрутить
+		*/
+		const slide_shift = slider.scrollLeft % slide_width
+		const max_scroll = slider.scrollWidth - slider.offsetWidth
+		let newval = slider.scrollLeft - slide_width
+		if (slide_shift) {
+			if (slide_width / 2 < slide_shift) { //Сдвиг дальше середины, мотаем влево
+				newval = newval + (slide_width - slide_shift) //Проматываем текущий и переходим на следующий
+			} else { //Ближе середины
+				newval = newval - slide_shift //Доматываем текущий
+			}
 		}
-		set(newval)
+
+		if (newval >= 0) {
+			return set(newval)
+		} else if (newval > - slide_width) {
+			return set(0)
+		} else {
+			return
+			//return set(0)
+		}
 	}
 	const next = () => {
-		const length = child.offsetWidth + gap
-		
-		let len = ((slider.scrollWidth - slider.offsetWidth + slider.scrollLeft) % length)
-		
+		const slide_width = child.offsetWidth + gap		
+		//console.log(slider.scrollWidth, slider.offsetWidth, slider.scrollLeft)
 		/*
-			slider.scrollWidth = 1000
-			slider.offsetWidth = 200
-			slider.scrollLeft = 10
-			length || child.offsetWidth + gap = 100
-			let = 1000 - 200 + 10 % 100 = 810 % 100 = 10
+			slider.scrollWidth = 4000  вся длина скрытая за прокруткой
+			slider.offsetWidth = 1295 видимая часть
+			slider.scrollLeft = 10 сколько уже прокрутили от начала
+			slide_width = 400 - ширина слайда
+			slide_shift = 10 % 400 = 10 - сдвиг от слайда
+			max_scroll = 4000 - 1295
+			newval = slider.scrollLeft = сколько надо прокрутить
 		*/
-		if (len < length * 1 / 2) len = length - len
-		else len = 2 * length - len
-		let newval = slider.scrollLeft + len
-		if (newval >= slider.scrollWidth - slider.offsetWidth - 10) {
-			direction = false
-			newval = slider.scrollWidth - slider.offsetWidth
+		const slide_shift = slider.scrollLeft % slide_width
+		const max_scroll = slider.scrollWidth - slider.offsetWidth
+		let newval = slider.scrollLeft + slide_width
+		if (slide_shift) {
+			if (slide_width / 2 < slide_shift) { //Сдвиг дальше середины
+				newval = newval + (slide_width - slide_shift) //Проматываем текущий и переходим на следующий
+			} else { //Ближе середины
+				newval = newval - slide_shift //Доматываем текущий
+			}
 		}
-		set(newval)
+
+		if (newval <= max_scroll) {
+			return set(newval)
+		} else if (newval <= max_scroll + slide_width) {
+			return set(max_scroll)
+		} else {
+			return
+			//return set(0)
+		}
+		
 	}
 	const go = () => {
 		if (!slider.closest('body')) return clearInterval(timer)
@@ -79,8 +111,8 @@ export default div => {
 	for (const left of lefts) left.addEventListener('click', prev)
 	for (const right of rights) right.addEventListener('click', next)
 
-	const timer = setInterval(go, 5000)
-	setTimeout(go, 5000)
+	const timer = setInterval(go, 6000)
+	setTimeout(go, 6000)
 
 	let startx, starty, diffx, diffy, drag, lastfocus, scrollLeft, scrollTop;
 
